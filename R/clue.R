4e148c39610bc9dd94d764c968a6f647d62125c8
cycle20 <46718432+cycle20@users.noreply.github.com>
2021-02-05 22:35:09 +0100

    clue.io API endpoint requests

--- R/clue.R
+++ R/clue.R
@@ -1,65 +1,168 @@
+##
+## clue.R: collect data from clue.io
+##
+## NOTE: clue.io API access requires registration
+##
+## API key is assigned to the clue.io user profile.
+## This script reads it from CLUE_USER_KEY environment variable.
+##
+
 library(httr)
 library(jsonlite)
 library(glue)
+library(assertthat)
+library(dplyr)
 
+##
+## Settings of global variables
+##
 USER_KEY <- Sys.getenv("CLUE_USER_KEY")
+## quick verification
+assertthat::assert_that(!is.null(USER_KEY) && nchar(USER_KEY) > 0)
 API_BASE <- "https://api.clue.io/api/"
 
-perts <- function(targets) {
-  apiFunction <- "perts"
+
+# TODO: rep_drug_indication
+# TODO: rep_drug_target
+# TODO: rep_fda_product
+# TODO: rep_fda_orange-book_term
+# TODO: rep_drug_moa
+# TODO: rep_fda_exclusivity
+
+#' Get drug-targets information from clue.io
+#'
+#' @param ... list of perturbation names (pert_iname)
+#'
+#' @return data.frame of the details.
+rep_drug_targets <- function(...) {
+  apiFunction <- "rep_drug_targets"
+
   filterParams <- list(
-    fields = c(
-      "pert_id",
-      "pert_iname",
-      "alt_name",
-      "pert_type",
-      "pert_summary",
-      "description",
-      #"clinical_phase",
-      "moa",
-      "status",
-      "target",
-      "pert_url",
-      "provisional",
-      "pubchem_cid"
-    ),
     where = list(
-      target = list(inq = targets)
+      name = list(inq = c(...))
     )
   )
-  filterParams["fields"] <- NULL
   filterParams <- jsonlite::toJSON(filterParams)
 
+  ## concatenate URL with parameters
+  requestUrl <- glue("{API_BASE}{apiFunction}?filter={filterParams}")
+  result <- getWithUserKey(requestUrl)
 
-  requestUrl <- glue(
-    "{API_BASE}{apiFunction}?",
-    "filter={filterParams}&user_key={USER_KEY}"
-  )
-  result <- httr::GET(requestUrl, verbose())
-
-  return(httr::content(result))
+  return(getJSONContentAsDataFrame(result))
 }
 
-resultAsJSON <- function() {
-  targets <- c(
-    "CXCR2"
-  #  "TP63",
-  #  "MMP7"
+#' Get drug information from clue.io
+#'
+#' Client side of rep_drugs API endpoint.
+#'
+#' @param ... list of internal (clue.io) perturbation identifiers.
+#'
+#' @return data.frame of the details.
+rep_drugs <- function(...) {
+  apiFunction <- "rep_drugs"
+
+  filterParams <- list(
+    where = list(
+      pert_iname = list(inq = c(...))
+    )
   )
+  filterParams <- jsonlite::toJSON(filterParams)
 
-  result <- perts(targets)
+  ## concatenate URL with parameters
+  requestUrl <- glue("{API_BASE}{apiFunction}?filter={filterParams}")
+  result <- getWithUserKey(requestUrl)
 
-  return(jsonlite::toJSON(result))
+  return(getJSONContentAsDataFrame(result))
 }
 
-# print(
-#   jsonlite::flatten(
-#     jsonlite::fromJSON( resultAsJSON()),
-#     recursive = TRUE
-#   )
-# )
+#' Get perturbation data from clue.io.
+#'
+#' Client side of perts API endpoint.
+#'
+#' @param ...
+#'
+#' @return data.frame of the details.
+#'         NOTE: Targets with no matches excluded from the result table.
+perts <- function(...) {
+  apiFunction <- "perts"
+  filterParams <- list(
+    where = list(
+      target = list(inq = c(...))
+    )
+  )
+  filterParams <- jsonlite::toJSON(filterParams)
 
-r <- resultAsJSON()
-browser()
+  ## concatenate URL with parameters
+  requestUrl <- glue("{API_BASE}{apiFunction}?filter={filterParams}")
+
+  response <- getWithUserKey(requestUrl)
+  responseFrame <- getJSONContentAsDataFrame(response)
+  ## selection of relevant fields
+  responseFrame <-
+    responseFrame %>%
+    select(
+      id,
+      target,
+      pert_iname,
+      moa,
+      description,
+      status,
+      pubchem_cid
+    )
+  return(responseFrame)
+}
+
+#' GET request
+#'
+#' @param requestUrl
+#'
+#' @return list object that represents HTTP response data
+getWithUserKey <- function(requestUrl) {
+  result <- httr::GET(url = requestUrl,
+    config = add_headers(user_key = USER_KEY), verbose())
+}
+
+#' Convert HTTP response body to data.frame
+#'
+#' Utility function, used by clue.io API calls.
+#'
+#' @param httrResponse result of an HTTP request performed by an httr function
+#'
+#' @return A data.frame object that contains data coerced from JSON response.
+getJSONContentAsDataFrame <- function(httrResponse) {
+  txt <- httr::content(httrResponse, as = "text")
+  list <- fromJSON(txt)
+  return(as.data.frame(list))
+}
 
 
+#' Download data from endpoints.
+#'
+#' Download and join data from different
+#' clue.io REST API endpoints.
+#'
+#' @return Final data.frame composed from multiple datasets.
+download <- function() {
+
+  # TODO: download data of these columns
+  # Target
+  # Compound name
+  # MoA
+  # Clinical Phase
+  # Disease Area
+  # Indication
+  # UniProt
+  # STRING link
+  # GeneCards
+  # Other External links
+  # Clinical Trials
+
+  durgTargets <- rep_drug_targets("CXCR2")
+  drugs <- rep_drugs(durgTargets$pert_iname)
+  browser()
+  # durgTargets %>% select(-c("id")) %>% left_join(drugs) %>% View()
+
+  return(result)
+}
+
+download()
