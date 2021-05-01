0c957369a0729469466df62b9e43e9e13a99c46c
cycle20 <46718432+cycle20@users.noreply.github.com>
2021-05-01 21:38:36 +0200

    merge script for cache directories #3

---
+++ src/gh_action__merge_caches.bash
@@ -0,0 +1,30 @@
+#
+# gh_action__merge_caches.bash:
+#
+# usage:
+#   bash gh_action__merge_caches.bash UNIPROT_CACHE_DIR ARTIFACT_CACHE_DIR
+#
+
+# cache dir contains UniProt "historical" files
+UNI_CACHE_DIR="$1"
+# extracted from GitHub artifcat or created by GitHub action
+GH_CACHE_DiR="$2"
+
+UNI_CACHE_FILE="$UNI_CACHE_DIR/UniProt_CacheLines.tsv"
+GH_CACHE_FILE="$GH_CACHE_DIR/cache.tsv"
+
+if [ -f "$GH_CACHE_FILE" ]; then
+  # exclude UniProt HTML lines
+  echo "Merge cache files"
+  {
+    egrep -v 'https://www.uniprot.org/uniprot/[A-Z0-9]{6,10}\s' "$GH_CACHE_FILE"
+    egrep '\.html\s' "$UNI_CACHE_FILE"
+  } > new_cache.tsv
+  cp new_cache.tsv "$GH_CACHE_FILE"
+  tail -30 "$GH_CACHE_FILE"
+else
+  # just set it as starter cache file
+  mkdir -p "$GH_CACHE_DIR"
+  mv -v "$UNI_CACHE_FILE" "$GH_CACHE_FILE"
+  head -30 "$GH_CACHE_FILE"
+fi
