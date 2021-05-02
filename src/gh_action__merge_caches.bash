38ce835f77ecc601bb98b6c40ccbea0fdce724cb
cycle20 <46718432+cycle20@users.noreply.github.com>
2021-05-02 18:52:51 +0200

    sandbox squash into development / Issue #3 (#19)
    * merge caches / Issue #3
    
    * get_artifact debug / Issue #3
    
    * shot happens - again?
    
    * shot happens 6
    
    * shot happens 7
    
    * merge script for cache directories #3
    
    * shot happens 8
    
    * cache early UniProt HTMLs / Issue #3
    
    * shot happens 9
    
    * shot happens 10
    
    * Merge of caches in CI / Issue #3
    
    * shot happens 11
    
    * shot happens 12
    
    * if dataPatch.R.tgz not found: just continue
    
     Issue #3
    
    * render pages in a workflow / Issue #3
    
    * let retention day be only 5
    
    Since it is just a sandbox.
    No need to keep artifact for longer period.
    
    * shot happens 13
    
    "friendly" globbing issue
    
    * lines from /proc/cpuinfo
    
    * shot happens 14
    
    well... this tar operation is not so simple... :)
    (it needs more research)
    
    * Render job in CI / Issue #3

---
+++ src/gh_action__merge_caches.bash
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+#
+# gh_action__merge_caches.bash:
+#
+# usage:
+#   bash gh_action__merge_caches.bash UNIPROT_CACHE_DIR ARTIFACT_CACHE_DIR
+#
+
+# cache dir contains UniProt "historical" files
+UNI_CACHE_DIR="$1"
+# extracted from GitHub artifcat or created by GitHub action
+GH_CACHE_DIR="$2"
+
+UNI_CACHE_FILE="$UNI_CACHE_DIR/UniProt_CacheLines.tsv"
+GH_CACHE_FILE="$GH_CACHE_DIR/cache.tsv"
+
+if [ -f "$GH_CACHE_FILE" ]; then
+  # exclude UniProt HTML lines
+  echo "Merge cache files"
+  {
+    grep -E -v 'https://www.uniprot.org/uniprot/[A-Z0-9]{6,10}\s' "$GH_CACHE_FILE"
+    grep -E '\.html\s' "$UNI_CACHE_FILE"
+  } > new_cache.tsv
+  cp new_cache.tsv "$GH_CACHE_FILE"
+  tail -30 "$GH_CACHE_FILE"
+else
+  # just set it as starter cache file
+  mkdir -p "$GH_CACHE_DIR"
+  mv -v "$UNI_CACHE_FILE" "$GH_CACHE_FILE"
+  head -30 "$GH_CACHE_FILE"
+fi
+
+# move HTML files into actual cache directory
+mv -v -t "$GH_CACHE_DIR" $UNI_CACHE_DIR/*.html
+tree "$GH_CACHE_DIR"
