857a8c2bf2ba757f299f9e25c9465c781eed0a02
cycle20 <46718432+cycle20@users.noreply.github.com>
2021-05-27 01:08:34 +0200

    "Calculated" STRING link by main: 9b53fee
    Related to Issue #36
    'scancer.js' was updated on 'main' branch.

--- js/scancer.js
+++ js/scancer.js
@@ -3,6 +3,9 @@
 //
 
 
+const root_url = "https://version-11-0b.string-db.org/";
+
+
 //
 // getSTRING: load STRING network image via dynamically added "img" tag
 function getSTRING(id) {
@@ -24,15 +27,39 @@ function getSTRING(id) {
     // concatenate key-value pairs: separator: '&'
     params = params.join("&");
 
-    root_url = "https://version-11-0b.string-db.org/";
     // get parent DOM element
     var stringDiv = document.getElementById(id);
     // Check for presence of 'img' element
     var imageCollection = stringDiv.getElementsByTagName("img");
-    if (imageCollection.length == 0) {
+    if (imageCollection.length === 0) {
+        var anchor = document.createElement("a");
+        anchor.text = `View network on ${root_url}`;
+        stringDiv.appendChild(anchor);
+        getNetworkLink(anchor, params);
+
+        link = `${root_url}api/svg/network?${params}`;
         var STRING_img = document.createElement("img");
-        STRING_img.src = `${root_url}api/svg/network?${params}`;
+        STRING_img.src = link;
         STRING_img.alt = `STRING network image of ${id}`;
         stringDiv.appendChild(STRING_img);
     }
+}
+
+
+function getNetworkLink(anchor, paramString) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var url = JSON.parse(this.responseText);
+      if (url.length > 0) {
+        anchor.href = url[0];
+        anchor.target = "_blank";
+      } else {
+        anchor.text = "FAILED XHR request...";
+      }
+    }
+  };
+  var link = `${root_url}api/json/get_link?${paramString}`;
+  request.open("GET", link, true);
+  request.send();
 }
