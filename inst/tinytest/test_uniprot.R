6fbd95ca17a471d7e05dd3e56cb8c955bec42dd4
cycle20 <46718432+cycle20@users.noreply.github.com>
2023-05-28 14:42:33 +0200

    Issue #54: UniProt test extended by Reactome, KEGG and STRING checks

--- inst/tinytest/test_uniprot.R
+++ inst/tinytest/test_uniprot.R
@@ -30,4 +30,15 @@ expect_equal(
   target = "positive regulation of MAPK cascade",
   current = clueTable$UniProtData[["P01133"]][["biologicalProcess"]][["GO:0043410"]]
 )
-# GO:0070062
+expect_equal(
+  target = "SHC1 events in ERBB2 signaling",
+  current = clueTable$UniProtData[["P01133"]][["Reactome"]][["R-HSA-1250196"]]
+)
+expect_equal(
+  target = "9606.ENSP00000265171",
+  current = clueTable$UniProtData[["P01133"]][["STRING"]]
+)
+expect_equal(
+  target = "hsa:1950",
+  current = clueTable$UniProtData[["P01133"]][["KEGG"]]
+)
